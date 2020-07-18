407efeb410a60a994faf38cc80f9005ede4515bf
ankurshishodia12 <ankur201996@gmail.com>
2020-07-18 08:54:54 +0000

    update file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -3,13 +3,13 @@
 dockerpath="ankurshishodia12/flaskapp"
 
 # Run in Docker Hub container with kubernetes
-kubectl run flaskskearlndemo\
+kubectl run flaskdemo\
     --generator=run-pod/v1\
     --image=$dockerpath\
-    --port=80 --labels app=flaskappdemo
+    --port=80 --labels app=flaskdemo
 
 # List kubernetes pods
 kubectl get pods
 
 # Forward the container port to host
-kubectl port-forward flaskappdemo 8000:80
+kubectl port-forward flaskdemo 8000:80
